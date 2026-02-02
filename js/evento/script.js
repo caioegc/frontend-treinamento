91ba9ae8165c3c7d48cf2938f3a2b85a19b4d092
Caio Eduardo Gomes Carvalho <dudueocara77@gmail.com>
2026-02-02 14:43:34 -0300

    feat: js

---
+++ js/evento/script.js
@@ -0,0 +1,24 @@
+function cliqueAqui(){
+    alert("Botão clicado");
+}
+
+function cliqueAquiDuplo(){
+    document.body.style.backgroundColor = "blue";
+}
+
+function virarVermelho(){
+ let cor = document.getElementById("teste");
+ cor.style.backgroundColor = "red";
+
+}
+
+function virarVioleta(){
+ let cor = document.getElementById("teste");
+ cor.style.backgroundColor = "blueviolet";
+
+}
+
+function limpaTexto(){
+document.getElementById("campoTexto").value = " ";
+
+}
---
+++ js/js/script.js
@@ -0,0 +1,12 @@
+document.getElementById("text").innerHTML = "Aprendendo <b>JS</b> do <b>zero</b>!";
+alert("Bem-vindo! Aqui é um alert()");
+console.log("Aqui é um console log")
+
+function soma(valor1, valor2){
+    return valor1 + valor2;
+
+}
+
+const valor = soma(15,20);
+
+alert(valor);
