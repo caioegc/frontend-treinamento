d9778e8eb7034ada6d2be7fd66fc74ee86c0b250
Caio Eduardo Gomes Carvalho <dudueocara77@gmail.com>
2026-02-02 17:47:04 -0300

    feat: DOM

---
+++ js/convertor/script.js
@@ -0,0 +1,6 @@
+
+function converter(){
+    const valor = document.getElementById("numero").value;
+    const valorConvertido = valor / 5.26;
+    alert("O valor em dolar é " + valorConvertido);
+}
