2562867a0ea8a13cb45c6d307a7dc10a2c9905ab
Caio Eduardo Gomes Carvalho <dudueocara77@gmail.com>
2026-02-02 19:42:51 -0300

    feat: forEach

--- js/convertor/script.js
+++ js/convertor/script.js
@@ -1,6 +1,21 @@
-
 function converter(){
     const valor = document.getElementById("numero").value;
     const valorConvertido = valor / 5.26;
     alert("O valor em dolar é " + valorConvertido);
 }
+
+
+
+
+const list = [
+{name: "rodolfo", age: 15},
+{name: "caio", age: 25},
+{name: "Luiz", age: 18},
+]
+
+list.forEach(function(valores){
+if(valores.age <= 18){
+    console.log(valores);
+}
+}
+)
