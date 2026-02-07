c3e1d09a92835a8adefd0b521061f42df4df9bba
Caio Eduardo Gomes Carvalho <dudueocara77@gmail.com>
2026-02-06 22:18:37 -0300

    feat: crud em JS

--- js2/exercicio2/script.js
+++ js2/exercicio2/script.js
@@ -1,28 +1,73 @@
 function mudarNome(){
-    let nome = "Caio"
+    let nome = "Caio";
     document.getElementById("ola").textContent = "Olá " +  nome;
 }
 
+let itens = [];
+
 function mostrarMensagem(){
     
-   let mensagem = "Mensagem salva com sucesso";
+   let mensagemSucesso = "Mensagem salva com sucesso";
    let inputText = document.getElementById("inputText");
-   let adicionarTarefa = inputText.value;
+   let adicionarTarefa = inputText.value.trim();
+   let mensagem = document.getElementById("mensagem");
+
     
    if(adicionarTarefa === ""){
-    document.getElementById("mensagem").textContent = "Por favor, digite uma tarefa!";
+    let mensagemErro = "Por favor, digite algo!"
+    mensagem.style.color = "#ff0000";
+    mensagem.textContent = mensagemErro
 } else{
-     document.getElementById("mensagem").textContent = mensagem;
-
-
-    let lista = document.getElementById("lista");
-    let novaTarefa = document.createElement("li");
-
-    novaTarefa.textContent = adicionarTarefa;
-
-    lista.appendChild(novaTarefa)
-
-    inputText.value = "";
+     mensagem.style.color = "#008000";
+     mensagem.textContent = mensagemSucesso;
+     itens.push(adicionarTarefa);  
+     renderizarTarefas();
 }
 
+    inputText.value = "";
+
+}
+
+function renderizarTarefas(){
+
+    let renderizarTarefas = document.getElementById("lista");
+
+    renderizarTarefas.innerHTML = "";
+
+
+    for(let i = 0; i < itens.length; i++){
+    let novaTarefa = document.createElement("li");
+    novaTarefa.textContent = itens[i];
+
+    let remover = document.createElement("button");
+    remover.textContent = "Remover"
+    remover.className = "removerBotao";
+
+    let editar = document.createElement("button");
+    editar.textContent = "Editar";
+    editar.className= "editarBotao";
+
+    remover.onclick = () => {removerTarefa(i)}
+    editar.onclick = () =>{editarTarefa(i)}
+
+    novaTarefa.appendChild(remover);
+    novaTarefa.appendChild(editar);
+    renderizarTarefas.appendChild(novaTarefa);
+
+}
+
+}
+
+function editarTarefa(i){
+    let tarefaEditar = prompt("Coloque a nova tarefa!");
+    if(tarefaEditar.trim() != ""){
+        itens[i] = tarefaEditar;
+        renderizarTarefas();
+    }
+}
+
+
+function removerTarefa(i){
+    itens.splice(i, 1);
+    renderizarTarefas();
 }
